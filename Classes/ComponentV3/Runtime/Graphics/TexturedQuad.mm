e206f2a7548e755a92cf5eca85a6f518991931b5
jsz <jszpilewski@me.com>
2010-12-20 13:16:27 +0100

    loads now BMFont .fnt files (watch out wit hiero ones!)

--- Classes/ComponentV3/Runtime/Graphics/TexturedQuad.mm
+++ Classes/ComponentV3/Runtime/Graphics/TexturedQuad.mm
@@ -557,6 +557,93 @@ namespace mx3
 			return 0;
 		
 		//info skip
+#define BUFFSIZE 1024
+		char buff[BUFFSIZE];
+		memset(buff,0x00,BUFFSIZE);
+		fgets(buff, BUFFSIZE, f_in);
+		
+		int packed;
+		
+		//DIFF 1: alphaChnl, .... - 4 more fields
+		
+		//header
+		int br = fscanf(f_in, "common lineHeight=%d base=%d scaleW=%d scaleH=%d pages=%d packed=%d alphaChnl=%d redChnl=%d greenChnl=%d blueChnl=%d\n",
+						&font->line_h, &font->base, &font->w, &font->h, &font->pages,&packed,&packed,&packed,&packed,&packed 
+						);
+		//page skip
+		
+		memset(buff,0x00,BUFFSIZE);
+		fgets(buff, BUFFSIZE, f_in);
+		
+		int page_id = 0;
+		br = sscanf(buff, "page%[ ]id=%d%[ ]file=\"%s\"%[ ]\n",spaces,&page_id,spaces,font->tex_filename,spaces);
+		
+		if (font->tex_filename[strlen(font->tex_filename)-1] == '"')
+			font->tex_filename[strlen(font->tex_filename)-1] = 0; //remove "
+		
+		//char count
+		memset(buff,0x00,BUFFSIZE);
+		fgets(buff, BUFFSIZE, f_in);
+		int chars_count = 0;
+		br = sscanf(buff, "chars%[ ]count=%d\n",spaces,&chars_count);
+		
+		
+		//chars
+		
+		//DIFF 3: i < chars_count (for hiero <=) oO
+		for (int i = 0; i <= chars_count; i++)
+		{
+			int char_id;
+			bm_char temp_char;
+			
+			memset(buff,0x00,BUFFSIZE);
+			fgets(buff, BUFFSIZE, f_in);
+			//printf("%s",buff);
+			//DIFF 2: no whitespace before \n. so br == 19 and not 20 like in hiero
+			br = sscanf(buff, "char id=%d%[ ]x=%d%[ ]y=%d%[ ]width=%d%[ ]height=%d%[ ]xoffset=%d%[ ]yoffset=%d%[ ]xadvance=%d%[ ]page=%d%[ ]chnl=%d\n",
+						&char_id, spaces,
+						
+						&temp_char.x, spaces,
+						&temp_char.y, spaces,
+						&temp_char.w, spaces,
+						&temp_char.h, spaces,
+						&temp_char.x_offset, spaces,
+						&temp_char.y_offset, spaces,
+						&temp_char.x_advance, spaces,
+						&temp_char.page, spaces,
+						&temp_char.chnl
+						);
+			if (br == 19)
+			{
+				font->chars[char_id] = temp_char;
+			}
+			else
+			{
+				break; //break the loop if we have no chars left (discard kerning, etc)
+				//abort();
+			}
+			
+		}
+		
+		//DIFF 4: we should parse the kerning info
+		//but we'll ignore it
+		
+	//	printf("\n##\n");
+		fclose (f_in);
+		return 1;
+	}
+
+	
+	int bm_loadfont_hiero (const char *filename, bm_font *font)
+	{
+		char spaces[100];
+		
+		
+		FILE *f_in = fopen(filename, "rt");
+		if (!f_in)
+			return 0;
+		
+		//info skip
 		char buff[512];
 		memset(buff,0x00,512);
 		fgets(buff, 512, f_in);
@@ -625,7 +712,7 @@ namespace mx3
 		fclose (f_in);
 		return 1;
 	}
-
+	
 	int bm_width (bm_font *font, char *text)
 	{
 		int w, l;
